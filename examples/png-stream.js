75cfba3145362fc53ab96c479e776001ae0c6109
Felix Geisendörfer <felix@debuggable.com>
2012-09-08 18:40:37 +0200

    Improve PngStream example

--- examples/png-stream.js
+++ examples/png-stream.js
@@ -1,11 +1,36 @@
-var PngStream = require('../lib/video/PngStream');
+var arDrone = require('..');
+var http    = require('http');
 
-var pngStream = new PngStream();
+console.log('Connecting png stream ...');
 
-console.log('Connecting ...');
-
+var pngStream = arDrone.createPngStream();
 pngStream.start();
 
-pngStream.on('data', function(pngBuffer) {
-  console.log('png', pngBuffer);
+
+var lastPng;
+pngStream
+  .on('connect', function() {
+    console.log('Connected');
+  })
+  .on('disconnect', function(err) {
+    console.log('Disconnected: ' + err);
+  })
+  .on('data', function(pngBuffer) {
+    lastPng = pngBuffer;
+  });
+
+var server = http.createServer(function(req, res) {
+  if (!pngStream.connected) {
+    res.writeHead(503);
+    res.end('PngStream is currently not connected.');
+    return;
+  }
+
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  res.end(lastPng);
 });
+
+server.listen(8080, function() {
+  console.log('Serving latest png on port 8080 ...');
+});
+
