abf48220d53a1bc87402144d730032ffb3035b5c
Felix Geisendörfer <felix@debuggable.com>
2012-09-18 16:47:00 +0200

    Rename

--- test/unit/control/test-AtCommandCreator.js
+++ test/unit/control/test-AtCommandCreator.js
@@ -6,45 +6,45 @@ var at               = require(common.lib + '/control/at');
 
 test('AtCommandCreator', {
   'command.number keeps incrementing': function() {
-    var writer = new AtCommandCreator();
+    var creator = new AtCommandCreator();
 
-    assert.equal(writer.ref().number, 0);
-    assert.equal(writer.ref().number, 1);
-    assert.equal(writer.ref().number, 2);
+    assert.equal(creator.ref().number, 0);
+    assert.equal(creator.ref().number, 1);
+    assert.equal(creator.ref().number, 2);
   },
 
   'raw': function() {
-    var writer = new AtCommandCreator();
+    var creator = new AtCommandCreator();
 
-    var cmd = writer.raw('FOO', 1, 2, 3);
+    var cmd = creator.raw('FOO', 1, 2, 3);
     assert.equal(cmd.type, 'FOO');
     assert.deepEqual(cmd.args, [1, 2, 3]);
   },
 
   'ref': function() {
-    var writer = new AtCommandCreator();
+    var creator = new AtCommandCreator();
 
-    var cmd = writer.ref();
+    var cmd = creator.ref();
     assert.equal(cmd.type, 'REF');
     assert.equal(cmd.args.length, 1);
     assert.equal(cmd.args[0], 0);
 
-    var cmd = writer.ref({takeoff: true});
+    var cmd = creator.ref({takeoff: true});
     assert.ok(cmd.args[0] & (1 << 9));
 
-    var cmd = writer.ref({emergency: true});
+    var cmd = creator.ref({emergency: true});
     assert.ok(cmd.args[0] & (1 << 8));
   },
 
   'pcmd': function() {
-    var writer = new AtCommandCreator();
+    var creator = new AtCommandCreator();
 
-    var cmd = writer.pcmd();
+    var cmd = creator.pcmd();
     assert.equal(cmd.type, 'PCMD');
     assert.equal(cmd.args.length, 5);
     assert.deepEqual(cmd.args, [0, 0, 0, 0, 0]);
 
-    var cmd = writer.pcmd({upDown: 0.9, leftRight: 0.8, frontBack: 0.7, clockWise: -0.5});
+    var cmd = creator.pcmd({upDown: 0.9, leftRight: 0.8, frontBack: 0.7, clockWise: -0.5});
     assert.ok(cmd.args.shift() & (1 << 0));
 
     assert.equal(cmd.args.shift(), at.floatString(0.8));
