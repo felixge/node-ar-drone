c0d219b41cb883ebf011996d53ff04fd4c3217ee
Laurent Eschenauer <laurent@eschenauer.be>
2013-05-28 21:03:02 +0200

    Added an example on saving the video-stream

---
+++ examples/save-video-stream.js
@@ -0,0 +1,20 @@
+// Run this to save a h264 video file, with the PaVE frame filtered out.
+// You can then use this file as a ffmpeg source for additional processing
+// or streaming to a ffserver
+
+var arDrone = require('..');
+var PaVEParser = require('../lib/video/PaVEParser'); 
+var output = require('fs').createWriteStream('./vid.h264');
+
+var video = arDrone.createClient().getVideoStream();
+var parser = new PaVEParser();
+
+parser
+  .on('data', function(data) {
+    output.write(data.payload);
+  })
+  .on('end', function() {
+    output.end();
+  });
+
+video.pipe(parser);
